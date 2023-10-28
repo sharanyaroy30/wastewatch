f890bc196191ac46fd99ef3f277360bdf71f13e9
Pragyan Yadav <72915754+ypragyan@users.noreply.github.com>
2023-10-27 23:21:59 -0400

    Update wastewatch.py

--- wastewatch.py
+++ wastewatch.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+# Set the title of the web app
+st.title("Simple Streamlit Example")
+
+# Add a button to the web app
+if st.button("Click me"):
+    st.write("You clicked the button!")
+
+# Display text
+st.write("This is a simple Streamlit web app.")
