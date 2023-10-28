a82df413a9c89254e2de11c8ee45b1b676fdeace
sharanyaroy30 <149217772+sharanyaroy30@users.noreply.github.com>
2023-10-27 20:30:08 -0700

    overview

--- wastewatch.py
+++ wastewatch.py
@@ -9,3 +9,26 @@ if st.button("Click me"):
 
 # Display text
 st.write("This is a simple Streamlit web app.")
+
+def overview_page():
+    st.title("Project Overview")
+    st.write("This is the overview page for your project.")
+    # Add your project overview content here
+
+# Create a function for another page
+def other_page():
+    st.title("Another Page")
+    st.write("This is another page of your Streamlit app.")
+    # Add content for this page here
+
+# Create a navigation menu
+page = st.sidebar.selectbox(
+    "Select a page",
+    ("Overview", "Another Page")
+)
+
+# Display the selected page
+if page == "Overview":
+    overview_page()
+elif page == "Another Page":
+    other_page()
